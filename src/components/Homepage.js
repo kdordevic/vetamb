d728d388d7fe4a95f1e1f161ed1af1ada39df33b
Katarina Dordevic <kejo79@gmail.com>
2024-05-15 18:17:04 +0200

    try to fix images

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -58,7 +58,7 @@ function Homepage() {
               },
             }}
             style={{
-              backgroundImage: `url("././photos/gallery/enterijer-6.JPG")`,
+              backgroundImage: `url("././photos/enterijer-6.JPG")`,
               backgroundPosition: "center",
             }}
           ></motion.div>
